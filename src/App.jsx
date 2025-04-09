9b8240c9481eb3f0d223a3ca0f85c9a0e87ca682
vmqxdv <vquintilio@gmail.com>
2025-04-09 17:25:29 +0200

    feat: Mostrata lista titoli articoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
-export default function App() {
+import articlesArray from './data/titles.json'
 
+
+const articleTitles = articlesArray.map(article => article.title);
+
+const renderItems = (titles) => {
+  return titles.map((title, i) => <li key={i}>{title}</li>);
+};
+
+let titlesToDisplay = renderItems(articleTitles);
+
+export default function App() {
   return (
-    <>
-    </>
+    <ul>
+      {titlesToDisplay}
+    </ul>
   )
 }
