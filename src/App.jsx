58ae348d747f9e072cefbb866d54a9fa03d68f1b
vmqxdv <vquintilio@gmail.com>
2025-04-09 17:59:59 +0200

    feat: Aggiunto form per aggiungere titoli alla lista

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,32 @@ import './App.css'
 
 import articlesArray from './data/titles.json'
 
-
-const articleTitles = articlesArray.map(article => article.title);
-
-const renderItems = (titles) => {
-  return titles.map((title, i) => <li key={i}>{title}</li>);
-};
-
-let titlesToDisplay = renderItems(articleTitles);
-
 export default function App() {
+  const initialTitles = articlesArray.map(article => article.title);
+  
+  const [titles, setTitles] = useState(initialTitles);
+  const [title, setTitle] = useState('');
+
+  const userFeedBack = (event) => {
+    event.preventDefault();
+    
+    if (title.trim() === '') return alert(`Input non valido`);
+
+    setTitles([...titles, title]);
+    alert(`"${title}" aggiunto alla lista!`);
+    setTitle('');
+  };
+
   return (
-    <ul>
-      {titlesToDisplay}
-    </ul>
+    <main>
+      <ul>
+        {titles.map((title, i) => <li key={i}>{title}</li>)}
+      </ul>
+
+      <form onSubmit={userFeedBack}>
+        <input type="text" value={title} onChange={e => setTitle(e.target.value)}/>
+        <button type="submit">Aggiungi</button>
+      </form>
+    </main>
   )
 }
